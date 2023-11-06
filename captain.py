55fde3ee145c319c75d05acd6c487f079bdf461f
Jarial <pankajjairal.tws@gmail.com>
2023-11-06 16:43:16 +0530

    project is done

--- captain.py
+++ captain.py
@@ -43,7 +43,6 @@ bard = Bard(token=token)
     wait_exponential_multiplier=1000,  # Wait for 1 second, then 2 seconds, 4 seconds, etc.
     stop_max_attempt_number=3,  # Maximum of 3 retries
 )
-
 def perform_search(query):
     try: 
         response = bard.get_answer(query)
@@ -62,7 +61,21 @@ def google_search(query):
     except ReadTimeout:
         return "Bard API request timed out. Please try again."
 
+# @client.event
+# async def on_message(message):
+#     if (message.author.bot == True):
+#         return 
+#     if  message.content.lower():
+#         query = message.content
+#         search_results = google_search(query)
+#         if search_results:
+#             await message.channel.send(search_results)
+#         else:
+#             await message.channel.send("No search results found.")
 
+#     await client.process_commands(message)
+
+# get description from image
 def generate_bard_image_description(file):
     try:
         prompt = "Explain this image"
@@ -87,13 +100,13 @@ async def on_message(message):
     global ticket_open
     print(message,"@@@@@")
     # if message.author.bot:
-    
-    if message.channel.name != "girolamo-chat":
-        return  # Ignore messages from other channels
     if message.author.bot or isinstance(message.channel, discord.DMChannel):
         return
+        # Check if the message is from the "girolamo-chat" channel
+    if message.channel.name != "girolamo-chat":
+        return  # Ignore messages from other channels
     
-    
+    content = message.content.lower()
     if content.startswith('/generate_image'):
         prompt = content[len('/generate_image'):].strip()
         
@@ -130,12 +143,12 @@ async def on_message(message):
             your_intro_prompt = f"""
             You are Girolamo, a large language model inspired by Italian Physicist Girolamo Cardano, is a versatile tool that can be used for many different purposes, including:
 
-            Learning: Girolamo can help users learn new things by providing them with information from a variety of sources, including books, articles, and websites.
+            Learning: Girolamo can help users learn new things by providing them with information from a variety of sources, including books, articles, and websites in english.
             Creation: Girolamo can help users create new content, such as poems, code, scripts, musical pieces, email, and letters.
             Entertainment: Girolamo can entertain users by telling stories, jokes, and playing games.
             Connection: Girolamo can help users connect with others by translating languages, writing letters, and generating creative content.
             Girolamo is still under development, but is learning and growing every day. Girolamo is committed to helping users in new and innovative ways.
-            And Please Reply in English.
+
             Prompt:
 
             User: {content}"""
@@ -177,4 +190,70 @@ async def on_error(event, *args, **kwargs):
         print(f"ConnectionClosed error: {exception}")
 
 client.run(discord_token)
+
+
+
+
+# openai.api_key = os.getenv('OPENAI_KEY')
+# def query(query):
+#     # Generate an image from a text description
+#     if query.startswith('/'):
+#         response = openai.Image.create(prompt=query)
+#         print('check redsponse',response.data)
+#         image_url = response['data'][0]['url']
+#         image_data = requests.get(image_url).content
+
+#         # Save the image to a file
+#         with open("image.png", "wb") as f:
+#             f.write(image_data)
+#         return image_data
+
+# client.run(discord_token)
+
+# def google_search(query):
+#     print('inside google search query',query)
+#     data = query.split('>')
+#     query_data = data[1]
+    
+#     # google_api_key = 'AIzaSyDSwL4umIZ4nYVsDJU76jzxzp_fvBHvGh4'
+#     google_api_key = 'bgiLwrJaZFCwxdjV-TKpSQ6DymyoH5BlrWMQ4ypmtCX7oIV1xsAfEd5LlBare0OvGHzTFQ.'
+#     # Replace 'YOUR_CX' with your actual Custom Search Engine ID (CX)
+#     cx = '3512329d56c5e41d4'
+#     base_url = 'https://www.googleapis.com/customsearch/v1'
+
+#     url = f'{base_url}?key={google_api_key}&cx={cx}&q={query_data}'
+
+#     response = requests.get(url)
+#     print(response,"check response")
+#     if response.status_code == 200:
+#         data = response.json()
+#         print('inside response',data)
+#         items = data.get('items', [])
+
+#         search_results = []
+#         max_results = 3  
+#         max_title_length = 100
+
+#         for i, item in enumerate(items[:max_results], start=1):
+#             title = item.get('title', f'Hotel {i}')
+#             link = item.get('link', 'No link available')
+#             snippet = item.get('snippet', 'No snippet available')
+
+#             # Truncate titles if they exceed the maximum length
+#             if len(title) > max_title_length:
+#                 title = title[:max_title_length] + '...' 
+
+#             # result_str = f'{i}. **{title}**\n{link}\n{snippet}\n'
+#             result_str = f'{i}. {title}'
+#             search_results.append(result_str)
+
+#         # Combine the search results into a single string
+#         formatted_results = '\n'.join(search_results)
+#         return formatted_results
+#     else:
+#         return f'Error: {response.status_code}\n{response.text}'
+    
+
+
+
 
